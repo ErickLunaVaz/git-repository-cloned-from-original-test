4ee00fe57e409138246f8f51e6193f5a13eb4ef9
Erick Luna <erickluna.25@gmail.com>
2020-04-10 19:38:11 -0700

    This commit will add a new funtcion just for fun

---
+++ behaviours.js
@@ -0,0 +1,15 @@
+function print(){
+    console.log("hello world");
+}
+
+function sayGoodBye(){
+    console.log("say good bye");
+}
+
+function practiceCreatingBranchFromASpecificCommit(){
+    console.log("this particular function was created while ");
+}
+
+function addeddFurFun(){
+    console.log("this function was addedd just fur fun");
+}
